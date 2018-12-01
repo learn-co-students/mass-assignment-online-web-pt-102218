5f3b3a57616488f11454349fa757243902c2dbbf
Paul Ashour <paul.ashour@gmail.com>
2018-12-01 23:51:30 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
-  #your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+  
+  def initialize(attributes)
+    attributes.each { |a, v| self.send("#{a}=", v) }
+  end
+  
+  def changeCode (null)
+    # change the code so that learn will accept my submission
+  end
 end
